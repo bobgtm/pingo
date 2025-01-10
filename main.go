ccc83fe68567259fdb1e8a35765c2bd5ad7961d6
bobgtm <robertgriffintm@gmail.com>
2025-01-10 02:53:55 +0000

    added method for getting local IP

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+// take a url or ip address
+// establish conn from host to remote url/ip
+// Discover the remote hostname and associated ip
+// send stream of raw/empty data
+// Check for a return
+// Print response from the remote host
+
+func getLocalAdress() net.IP {
+	con, error := net.Dial("udp", "8.8.8.8:80")
+	if error != nil {
+		log.Fatal(error)
+		defer con.Close()
+	}
+	localAddress := con.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP
+
+}
 
 func main() {
-	fmt.Println("vim-go")
+	ipString := getLocalAdress()
+	fmt.Println(ipString)
 }
